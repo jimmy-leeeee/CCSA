f4696e9524da913d10cc08fb704a088727579669
J immy <jimmy_only@JimmyMB.local>
2025-12-09 10:47:29 +0800

    init commit

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_cors import CORS
+from app.routers.stations import stations_bp
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+
+    # /api/stations
+    app.register_blueprint(stations_bp, url_prefix="/api")
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
